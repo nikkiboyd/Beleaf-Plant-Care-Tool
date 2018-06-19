5fe018ef90f5b8b62b1aea5d74910331163645fb
Guest User <Guest@Epicodus-7B.local>
2018-06-19 10:02:24 -0700

    add js for form to reset after 'create your own' selection and input more pre-set plants

--- js/scripts.js
+++ js/scripts.js
@@ -45,10 +45,27 @@ function makeSchedule(taskArrayWater) {
 makeSchedule(taskArrayWater)
 
 
-
+//pair one
 var allPlantTemplates = []
 
+//user logic
 $(function(){
+  document.getElementById("waterSelection").onchange = function(){
+    var elementId = "#waterSelection"
+    showHideMonthWeek(elementId)
+  };
+
+  document.getElementById("pruningSelection").onchange = function(){
+    var elementId = "#pruningSelection"
+    showHideMonthWeek(elementId)
+  };
+
+  document.getElementById("fertilizingSelection").onchange = function(){
+    var elementId = "#fertilizingSelection"
+    showHideMonthWeek(elementId)
+  };
+
+
   document.getElementById("selectPlant").onchange = function(){
     var plantName = $("#selectPlant :selected").text()
     console.log(plantName)
@@ -62,26 +79,38 @@ $(function(){
        $("#sunlightSelection").val(sunlight);
        $("#hardinessSelection").val(hardiness);
        $("#waterSelection").val(waterFrequency);
+       showHideMonthWeek("#waterSelection");
        $("#pruningSelection").val(pruningFrequency);
+       showHideMonthWeek("#pruningSelection");
        $("#fertilizingSelection").val(fertilizingFrequency);
-      }
-    }
-  };
-    document.getElementById("waterSelection").onchange = function(){
-    var monthSelection = $("#waterSelection :selected").text()
-    if (monthSelection === "Once a month") {
-      $("#waterWeekdaySelection").hide();
-      $("#waterMonthSelection").show();
-      console.log("in the if section")
-    } else {
-      $("#waterMonthSelection").hide();
-      $("#waterWeekdaySelection").show();
+       showHideMonthWeek("#fertilizingSelection");
+     } else if (plantName === "Create your own") {
+        document.getElementById("plantEntryForm").reset();
+        showHideMonthWeek("#waterSelection");
+        showHideMonthWeek("#pruningSelection");
+        showHideMonthWeek("#fertilizingSelection");
+     }
     }
   };
 });
 
 
+function showHideMonthWeek(elementId){
+  var monthSelection = $(elementId + " :selected").text()
+  if (monthSelection === "Once a month") {
+    $(elementId + "Weekday").hide();
+    $(elementId + "Month").show();
+  } else if (monthSelection.search("week") > -1) {
+    $(elementId + "Month").hide();
+    $(elementId + "Weekday").show();
+  } else {
+    $(elementId + "Month").hide();
+    $(elementId + "Weekday").hide();
+  }
+}
 
+
+//business logic
 function Plant(commonName, sunlight, hardiness, water, pruning, fertilizing){
   this.commonName = commonName
   this.sunlight = sunlight
@@ -93,11 +122,25 @@ function Plant(commonName, sunlight, hardiness, water, pruning, fertilizing){
 
 var spider = new Plant ("Spider Plant", "Part Sun", "Very Tolerant", ["Every other week"], ["Once a month"], ["Every other week"])
 allPlantTemplates.push(spider)
+var snake = new Plant ("Snake Plant", "Indirect Sun", "Very Tolerant", ["Once a month"], ["Never"], ["Annually"])
+allPlantTemplates.push(snake)
+var maple = new Plant ("Japanese Maple", "Part Sun", "Temperamental", ["Twice a week"], ["As needed"], ["Never"])
+allPlantTemplates.push(maple)
+var xmascactus = new Plant ("Christmas Cactus", "Shade", "Average", ["Once a week"], ["Once a month"], ["Every other week"])
+allPlantTemplates.push(xmascactus)
+var aralia = new Plant ("Japanese Aralia", "Shade", "Average", ["Once a week"], ["Once a month"], ["Once a month"])
+allPlantTemplates.push(aralia)
+var peacelily = new Plant ("Peace Lily", "Indirect Sun", "Very Tolerant", ["Twice a week"], ["Once a month"], ["Every other week"])
+allPlantTemplates.push(peacelily)
+var asparagus = new Plant ("Asparagus Fern", "Part Sun", "Temperamental", ["Once a week"], ["Once a month"], ["Once a month"])
+allPlantTemplates.push(asparagus)
+var dracena = new Plant ("Dracena", "Indirect Sun", "Very Tolerant", ["Every other week"], ["As needed"], ["Every other week"])
+allPlantTemplates.push(dracena)
 
-//test user input
-var spidey = new Plant ("spider", "full", ["weekly"])
-spidey.nickname = "spidey"
-spidey.location = "next to window"
+// //test user input
+// var spidey = new Plant ("spider", "full", ["weekly"])
+// spidey.nickname = "spidey"
+// spidey.location = "next to window"
 
 
 
